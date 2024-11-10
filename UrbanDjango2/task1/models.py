9449149eb2c92de8b45744d858650752f1ededf2
paspekov34 <nikpaspekov17@mail.ru>
2024-11-10 20:36:37 +0300

    Сайт администрирования

--- UrbanDjango2/task1/models.py
+++ UrbanDjango2/task1/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Buyer(models.Model):
-    objects = None
     name = models.CharField(max_length=70)
     balance = models.DecimalField(max_digits=100000, decimal_places=2)
     age = models.IntegerField()
@@ -10,10 +9,16 @@ class Buyer(models.Model):
         return self.name
 
 class Game(models.Model):
-    objects = None
     title = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=10000, decimal_places=2)
     size = models.DecimalField(max_digits=100000, decimal_places=3)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='buyers')
+
+    def update(self):
+        # Логика обновления игры
+        pass
+
+
+
